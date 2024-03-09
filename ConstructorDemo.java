8f0b564ea24cb5dd9401c1a8a9a9c280526ace50
jigarthakkar123 <jigar.thakkar.tops@gmail.com>
2024-03-09 09:16:15 +0530

    9-3-24 Updated

---
+++ ConstructorDemo.java
@@ -0,0 +1,17 @@
+package com.core;
+
+class Box
+{
+	double width,height,depth;
+	Box()
+	{
+		System.out.println("Default Constructor Called");
+	}
+}
+
+public class ConstructorDemo {
+
+	public static void main(String[] args) {
+		Box b1=new Box();
+	}
+}
